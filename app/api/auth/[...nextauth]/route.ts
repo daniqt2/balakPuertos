ade3ce7e5749a52db1ee62ed7544e82b7aa97843
Dani Quesada <daniela.dudyfit@gmail.com>
2023-07-19 18:24:39 +0200

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
