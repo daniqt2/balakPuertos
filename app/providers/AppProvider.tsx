1c3b8f2e1df0c8de307655acf754257ab84e9fee
Dani Quesada <daniela.dudyfit@gmail.com>
2023-07-19 18:29:38 +0200

    required

--- app/providers/AppProvider.tsx
+++ app/providers/AppProvider.tsx
@@ -8,7 +8,7 @@ import Login from "../components/Login";
 const AppContext = createContext({});
 
 export function AppWrapper({ children }: { children: React.ReactNode }) {
-  const { data: session } = useSession({});
+  const { data: session } = useSession({ required: true });
 
   if (!session) return <Login />;
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
